fd440c613b39313a17e0dedd499986e5cee2d9e8
allen9408 <724330304@qq.com>
2016-12-02 09:16:24 -0500

    add new case in ./r.sh
    Signed-off-by: allen9408 <724330304@qq.com>

--- r.sh
+++ r.sh
@@ -29,4 +29,16 @@ elif [[ $pred_type == '2lev' ]]; then
 	# 2 level predictor
 	# 4 parameters: l1size l2size hist_size xor
 	./sim-outorder -bpred 2lev -bpred:2lev $2 $3 $4 $5 ./tests/bin/test-math
+elif [[ $pred_type == 'hodge' ]]; then
+	# hodgepodge predictor
+	# 2 parameters: history_reg_size associativity
+	hist_size=$2
+	assoc=$3
+	if [[ -z $hist_size ]]; then
+		hist_size=10
+	fi
+	if [[ -z $assoc ]]; then
+		assoc=2
+	fi
+	./sim-outorder -bpred hodge -bpred:hodge $hist_size $assoc ./tests/bin/test-math
 fi
