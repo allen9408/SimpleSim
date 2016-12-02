512d0431b9f4b8898c565ec9fb8dcda6f217be9b
allen9408 <724330304@qq.com>
2016-12-02 09:29:13 -0500

    Add benchmark choice
    Signed-off-by: allen9408 <724330304@qq.com>

--- r.sh
+++ r.sh
@@ -11,20 +11,30 @@ echo PREDICTORTYPE:$pred_type
 if [[ $pred_type == 'hash' ]]; then
 	# hash predictor
 	hash_size=$2
+	bench=$3
 	if [[ -z $hash_size ]]; then
 		# default size 512
 		hash_size=512
 	fi
-	./sim-outorder -bpred hash -bpred:hash $hash_size ./tests/bin/test-math
+	if [[ -z $bench ]]; then
+		#statements
+		bench='test-math'
+	fi
+	./sim-outorder -bpred hash -bpred:hash $hash_size ./tests/bin/$bench
 elif [[ $pred_type == 'gshare' ]]; then
 	# gshare predictor
 	# 1 parameter: history FIFO size
 	fifo_size=$2
+	bench=$3
 	if [[ -z $fifo_size ]]; then
 		# default size 512
 		fifo_size=10
 	fi
-	./sim-outorder -bpred gshare -bpred:gshare $fifo_size ./tests/bin/test-math
+	if [[ -z $bench ]]; then
+		#statements
+		bench='test-math'
+	fi
+	./sim-outorder -bpred gshare -bpred:gshare $fifo_size ./tests/bin/$bench
 elif [[ $pred_type == '2lev' ]]; then
 	# 2 level predictor
 	# 4 parameters: l1size l2size hist_size xor
@@ -34,11 +44,16 @@ elif [[ $pred_type == 'hodge' ]]; then
 	# 2 parameters: history_reg_size associativity
 	hist_size=$2
 	assoc=$3
+	bench=$4
 	if [[ -z $hist_size ]]; then
 		hist_size=10
 	fi
 	if [[ -z $assoc ]]; then
 		assoc=2
 	fi
-	./sim-outorder -bpred hodge -bpred:hodge $hist_size $assoc ./tests/bin/test-math
+	if [[ -z $bench ]]; then
+		#statements
+		bench='test-math'
+	fi
+	./sim-outorder -bpred hodge -bpred:hodge $hist_size $assoc ./tests/bin/$bench
 fi
