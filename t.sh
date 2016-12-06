1ff13829478fc17de95fe46d9082002532c4498e
allen9408 <724330304@qq.com>
2016-12-06 11:49:14 -0500

    Modify testing script
    Signed-off-by: allen9408 <724330304@qq.com>

--- t.sh
+++ t.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+hash_config="2^$1"|bc
+gsahre_config=$1
+hodge_config1=$1
+hodge_config2=$2
 for file in ./tests/bin/*; do
   echo ${file##*/}
   echo $file
-  ./r.sh hash 1024 $file >./test_report/hash_10_${file##*/}
-  ./r.sh gshare 10 $file >./test_report/gshare_10_${file##*/}
-  ./r.sh hodge 10 2 $file >./test_report/hodge_10_1_${file##*/}
+  ./r.sh hash $hash_config $file >./test_report/hash_$1_$target 2>&1 
+  ./r.sh gshare $gsahre_config $file >./test_report/gshare_$1_$target 2>&1
+  ./r.sh hodge $hodge_config1 $hodge_config2 $file >./test_report/hodge_$1_$2_$target 2>&1
 done
