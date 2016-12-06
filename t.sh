f59f2b9fe849a77d14a7099afc70ff9b200cd8c8
allen9408 <724330304@qq.com>
2016-12-06 09:10:32 -0500

    Modify testing script
    Signed-off-by: allen9408 <724330304@qq.com>

--- t.sh
+++ t.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 for file in ./tests/bin/*; do
-  echo ${file##*/}
-  echo $file
-  ./r.sh hash 1024 $file >./test_report/hash_10_${file##*/}
-  ./r.sh gshare 10 $file >./test_report/gshare_10_${file##*/}
-  ./r.sh hodge 10 2 $file >./test_report/hodge_10_1_${file##*/}
+  target=${file##*/}
+  echo $target
+  ./r.sh hash 1024 $target >./test_report/hash_10_$target 2>&1
+  ./r.sh gshare 10 $target >./test_report/gshare_10_$target 2>&1
+  ./r.sh hodge 10 2 $target >./test_report/hodge_10_1_$target 2>&1
 done
