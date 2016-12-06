e9c7a77e69697631d289475b3fa293e2494b3a83
allen9408 <724330304@qq.com>
2016-12-06 14:32:14 -0500

    Update test report
    Signed-off-by: allen9408 <724330304@qq.com>

--- t.sh
+++ t.sh
@@ -4,9 +4,9 @@ gsahre_config=$1
 hodge_config1=$1
 hodge_config2=$2
 for file in ./tests/bin/*; do
-  echo ${file##*/}
+  target=${file##*/}
   echo $file
-  ./r.sh hash $hash_config $file >./test_report/hash_$1_$target 2>&1 
-  ./r.sh gshare $gsahre_config $file >./test_report/gshare_$1_$target 2>&1
-  ./r.sh hodge $hodge_config1 $hodge_config2 $file >./test_report/hodge_$1_$2_$target 2>&1
+  ./r.sh hash $hash_config $target >./test_report/hash_$1_$target 2>&1 
+  ./r.sh gshare $gsahre_config $target >./test_report/gshare_$1_$target 2>&1
+  ./r.sh hodge $hodge_config1 $hodge_config2 $target >./test_report/hodge_$1_$2_$target 2>&1
 done
